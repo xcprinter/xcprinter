cf5ae13ba36b1640ff444323236515c34a19a781
qinmingyuan <mingyuan0715@foxmail.com>
2026-05-16 15:23:14 +0800

    package

--- index.mjs
+++ index.mjs
@@ -1,5 +1,4 @@
 import PrintCPCL from 'src/print_cpcl'
 import PrintPOS from 'src/print_pos'
 import PrintPic from 'src/print_pic'
-
-module.exports = { PrintCPCL, PrintPOS, PrintPic }
+export { PrintCPCL, PrintPOS, PrintPic }
