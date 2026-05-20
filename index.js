c5d02c6812d90e18a30ea7e24a2a0c70f88393a7
qinmingyuan <mingyuan0715@foxmail.com>
2026-05-20 15:54:00 +0800

    index

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import PrintCPCL from 'src/print_cpcl'
-import PrintPOS from 'src/print_pos'
-import PrintPic from 'src/print_pic'
+import PrintCPCL from './src/print_cpcl'
+import PrintPOS from './src/print_pos'
+import PrintPic from './src/print_pic'
 
 module.exports = { PrintCPCL, PrintPOS, PrintPic }
